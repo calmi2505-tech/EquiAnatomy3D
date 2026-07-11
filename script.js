8dc198577d1acfdc3987e0911e07c583083c9200
calmi2505-tech <calmi2505@gmail.com>
2026-07-11 18:35:33 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
-document.addEventListener("DOMContentLoaded", () => {
-    console.log("EquiAnatomy 3D iniciado");
+const canvas = document.getElementById("canvas3d");
 
-    alert("🐎 Bienvenido a EquiAnatomy 3D");
-});
+canvas.innerHTML = `
+<div style="
+display:flex;
+justify-content:center;
+align-items:center;
+width:100%;
+height:100%;
+flex-direction:column;
+color:white;
+font-size:24px;
+">
+🐎
+<h2>Visor 3D de EquiAnatomy</h2>
+<p>Preparando motor gráfico...</p>
+</div>
+`;
+
+console.log("EquiAnatomy 3D iniciado");
